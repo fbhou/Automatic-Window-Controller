3883d72ffc6455b61bce0737ee799c2ad87de639
Uranus <whlisikuang@outlook.com>
2020-12-16 22:40:57 +0800

    refactor(GUI): refactor classes of weather part
    refactor classes to be more readable

--- GUI/weather.cpp
+++ GUI/weather.cpp
@@ -10,13 +10,42 @@
 #include<QString>
 #include<QtNetwork>
 
-Weather::Weather(QWidget *parent)
+
+WeatherCondition::WeatherCondition(const QString &CurrentDateTime,const QString &UpdateDateTime,
+                                   const QString &area,const QString &WeatherType,const QString &temperature,
+                                   const QString &WindDirection,const QString &WindPower,const QString &humidity)
+{
+    this->CurrentDateTime=CurrentDateTime;
+    this->UpdateDateTime=UpdateDateTime;
+    this->area=area;
+    this->WeatherType=WeatherType;
+    this->temperature=temperature;
+    this->WindDirection=WindDirection;
+    this->WindPower=WindPower;
+    this->humidity=humidity;
+}
+
+QString WeatherCondition::ToString() const
+{
+    QString str="";
+    str.append("获取时间："+this->CurrentDateTime+"\n");
+    str.append("更新时间："+this->UpdateDateTime+"\n");
+    str.append("地区："+this->area+"\n");
+    str.append("天气："+this->WeatherType+"\n");
+    str.append("气温："+this->temperature+"\n");
+    str.append("风向："+this->WindDirection+"\n");
+    str.append("风速："+this->WindPower+"\n");
+    str.append("湿度："+this->humidity);
+    return str;
+}
+
+bool WeatherDataCollector::init(QWidget *parent)
 {
     QFile LoadConfig("config.json");
     if(!LoadConfig.open(QIODevice::ReadOnly))
     {
         Correction::WindowsPrint(parent,"Could't open Config json");
-        return;
+        return false;
     }
     QByteArray Data = LoadConfig.readAll();
     LoadConfig.close();
@@ -25,14 +54,15 @@ Weather::Weather(QWidget *parent)
     if(JsonError.error!=QJsonParseError::NoError)
     {
         Correction::WindowsPrint(parent,"Json error!");
-        return;
+        return false;
     }
     QJsonObject JsonObj=JsonDoc.object();
     UrlString=JsonObj["url"].toString();
     ApiString=JsonObj["APIKey"].toString();
+    return true;
 }
 
-QByteArray Weather::GetInfo(QWidget *parent)
+bool WeatherDataCollector::update(QWidget *parent)
 {
     QNetworkAccessManager *manager=new QNetworkAccessManager();
     QNetworkReply *reply=manager->get(QNetworkRequest(QUrl(UrlString+ApiString)));
@@ -47,32 +77,28 @@ QByteArray Weather::GetInfo(QWidget *parent)
     else
     {
         Correction::WindowsPrint(parent,reply->errorString());
+        return false;
     }
-    return ResponseData;
-}
-
-QString Weather::UpdateWeather(QWidget *parent)
-{
-    QByteArray data=this->GetInfo(parent);
     QJsonParseError JsonError;
-    QJsonDocument JsonDoc(QJsonDocument::fromJson(data,&JsonError));
+    QJsonDocument JsonDoc(QJsonDocument::fromJson(ResponseData,&JsonError));
     if(JsonError.error!=QJsonParseError::NoError)
     {
         Correction::WindowsPrint(parent,"Can't analyse the data collected.");
-        return "";
+        return false;
     }
     QJsonObject JsonObj=JsonDoc.object();
     QJsonArray arrayValue=JsonObj.value(QStringLiteral("lives")).toArray();
-    QJsonObject WeatherCondition=arrayValue.at(0).toObject();
-    QString str="";
-    str.append("获取时间："+QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")+"\n");
-    str.append("更新时间："+WeatherCondition["reporttime"].toString()+"\n");
-    str.append("地区："+WeatherCondition["province"].toString()+
-               WeatherCondition["city"].toString()+"\n");
-    str.append("天气："+WeatherCondition["weather"].toString()+"\n");
-    str.append("气温："+WeatherCondition["temperature"].toString()+"\n");
-    str.append("风向："+WeatherCondition["winddirection"].toString()+"\n");
-    str.append("风速："+WeatherCondition["windpower"].toString()+"\n");
-    str.append("湿度："+WeatherCondition["humidity"].toString()+"\n");
-    return str;
+    QJsonObject WeatherCollected=arrayValue.at(0).toObject();
+    this->Condition=WeatherCondition(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"),
+                                     WeatherCollected["reporttime"].toString(),
+                                     WeatherCollected["province"].toString()+WeatherCollected["city"].toString(),
+                                     WeatherCollected["weather"].toString(),WeatherCollected["temperature"].toString(),
+                                     WeatherCollected["winddirection"].toString(),WeatherCollected["windpower"].toString(),
+                                     WeatherCollected["humidity"].toString());
+    return true;
+}
+
+QString WeatherDataCollector::ToString() const
+{
+    return this->Condition.ToString();
 }
