5ae6eac903a5dc8801fe28af0b05f183b6f7a50f
Uranus <whlisikuang@outlook.com>
2020-12-24 12:09:39 +0800

    feat(Arduino): add Arduino part with Mind+ code
    use Arduino to control the machine

---
+++ Arduino/open.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Mind+\Arduino\hardware\tools\avr\bin\avrdude" -C"C:\Program Files (x86)\Mind+\Arduino/hardware/tools/avr/etc/avrdude.conf" -v -patmega328p -carduino -PCOM5 -b115200 -D -Uflash:w:"open.hex":i
